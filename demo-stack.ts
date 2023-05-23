98d39e500e932b18e8f4b39cceff4848596be858
Sebastian Richter <sebastian.max.richter@gmail.com>
2023-05-23 09:50:28 +0200

    Implement step 1

--- demo-stack.ts
+++ demo-stack.ts
@@ -1,4 +1,13 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnProject } from "@mongodbatlas-awscdk/project";
+import { CfnTypeActivation, Stack, StackProps } from "aws-cdk-lib";
+import {
+  Role,
+  CompositePrincipal,
+  ServicePrincipal,
+  PolicyDocument,
+  PolicyStatement,
+  Effect,
+} from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
 const REGION = "eu-west-1";
@@ -12,5 +21,70 @@ const VPC = {
 export class DemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // prerequisites
+
+    // execution role
+    // https://github.com/mongodb/mongodbatlas-cloudformation-resources/blob/master/examples/execution-role.yaml
+    const executionRole = new Role(this, "DemoStackMongoDBAtlasExecutionRole", {
+      roleName: "DemoStackMongoDBAtlasExecutionRole",
+      assumedBy: new CompositePrincipal(
+        new ServicePrincipal("cloudformation.amazonaws.com"),
+        new ServicePrincipal("resources.cloudformation.amazonaws.com"),
+        new ServicePrincipal("lambda.amazonaws.com")
+      ),
+      inlinePolicies: {
+        ResourceTypePolicy: new PolicyDocument({
+          assignSids: true,
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                "secretsmanager:CreateSecret",
+                "secretsmanager:CreateSecretInput",
+                "secretsmanager:DescribeSecret",
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:PutSecretValue",
+                "secretsmanager:UpdateSecretVersionStage",
+                "ec2:CreateVpcEndpoint",
+                "ec2:DeleteVpcEndpoints",
+                "cloudformation:CreateResource",
+                "cloudformation:DeleteResource",
+                "cloudformation:GetResource",
+                "cloudformation:GetResourceRequestStatus",
+                "cloudformation:ListResources",
+                "cloudformation:UpdateResource",
+                "iam:AttachRolePolicy",
+                "iam:CreateRole",
+                "iam:DeleteRole",
+                "iam:GetRole",
+                "iam:GetRolePolicy",
+                "iam:ListAttachedRolePolicies",
+                "iam:ListRolePolicies",
+                "iam:PutRolePolicy",
+              ],
+              resources: ["*"],
+            }),
+          ],
+        }),
+      },
+    });
+
+    // extensions
+    [CfnProject.CFN_RESOURCE_TYPE_NAME]
+      .map((type) => type.replace(/::/g, "-"))
+      .map(
+        (type) =>
+          new CfnTypeActivation(this, `DemoStackTypeActiviation${type}`, {
+            publicTypeArn: `arn:aws:cloudformation:${REGION}::type/resource/bb989456c78c398a858fef18f2ca1bfc1fbba082/${type}`,
+            executionRoleArn: executionRole.roleArn,
+          })
+      );
+
+    // atlas project
+    const demoAtlasProject = new CfnProject(this, "DemoStackAtlasProject", {
+      name: "Demo",
+      orgId: ATLAS_ORG_ID,
+    });
   }
 }
